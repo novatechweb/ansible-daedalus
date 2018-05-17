d6678161def129a2d58d2f92d3d0560b89cdcac0
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-05-17 14:39:04 -0500

    Add environment variables for parameters

--- docker/buildbot-master/buildbot/start.sh
+++ docker/buildbot-master/buildbot/start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-mkdir -p $HOME/.ssh
-cp -R /run/secrets/ssh/* $HOME/.ssh
-chmod -R 600 $HOME/.ssh/*
+BUILDBOT_DATA=${BUILDBOT_DATA-"/buildbot"}
 
-ln -v -s -f /home/buildbot/buildbot.tac /home/buildbot/master.cfg /home/buildbot/*.py /buildbot
+mkdir -p ${HOME}/.ssh
+cp -R /run/secrets/ssh/* ${HOME}/.ssh
+chmod -R 600 ${HOME}/.ssh/*
+
+ln -v -s -f ${HOME}/buildbot.tac ${HOME}/master.cfg ${HOME}/*.py $BUILDBOT_DATA
 
 /usr/bin/buildbot --verbose upgrade-master
 exec twistd --nodaemon --logfile=- --pidfile=/tmp/twistd.pid --python=buildbot.tac
