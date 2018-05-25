43b0b44ef20e50b64525c99f09522bb19461a0f5
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-05-25 10:36:27 -0500

    Refresh of TESTING--VirtualMachine

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 #PYTHONUNBUFFERED=1 \
      
-
+ANSIBLE_ENABLE_TASK_DEBUGGER=True \
 ANSIBLE_FORCE_COLOR=true \
 ANSIBLE_HOST_KEY_CHECKING=false \
 ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -o ControlMaster=auto -o ControlPersist=60s' \
