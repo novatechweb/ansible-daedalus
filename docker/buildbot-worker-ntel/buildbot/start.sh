9afd1dea63dafe9470bc8f6e0ba1a7b38a8a08b3
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-05-31 16:30:27 -0500

    Refresh of buildbot-worker-ntel

--- docker/buildbot-worker-ntel/buildbot/start.sh
+++ docker/buildbot-worker-ntel/buildbot/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-mkdir -p $HOME/.ssh
-cp -R /run/secrets/ssh/* $HOME/.ssh
-chmod -R 600 $HOME/.ssh/*
+SSH_BITS=${SSH_BITS-"2048"}
+SSH_TYPE=${SSH_TYPE-"rsa"}
+SSH_FILE=${SSH_FILE-"$HOME/.ssh/id_rsa"}
 
-ln -v -s -f /home/buildbot/buildbot.tac /buildbot/buildbot.tac
+BUILDBOT_DATA=${BUILDBOT_DATA-"/buildbot"}
+
+ssh-keygen -q -b ${SSH_BITS} -t ${SSH_TYPE} -N '' -f ${SSH_FILE} 0>&- 1>/dev/null 2>/dev/null
+
+ln -v -s -f ${HOME}/buildbot.tac $BUILDBOT_DATA
 exec twistd --nodaemon --logfile=- --pidfile=/tmp/twistd.pid --python=buildbot.tac
