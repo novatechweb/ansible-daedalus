697a5a15d36d015cc6826bcf172cc19c65e07021
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-06-04 16:33:45 -0500

    Refresh of TESTING--VirtualMachine

--- provision.sh
+++ provision.sh
@@ -9,7 +9,6 @@ ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -o Contr
 exec ansible-playbook \
         --connection=ssh \
         --timeout=30 \
-        --limit="vmdaedalus.novatech-llc.com" \
         --inventory-file=/home/coopera/Developer/testdaedalus/vmdaedalus/vagrant_ansible_inventory \
-        -v \
+        -vv \
         ansible-playbook/site.yml "$@"
