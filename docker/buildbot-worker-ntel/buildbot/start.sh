df9873384eab8d22e265831f92e5ff5da4fde663
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-07-10 14:30:42 -0500

    Refresh of refresh-temp

--- docker/buildbot-worker-ntel/buildbot/start.sh
+++ docker/buildbot-worker-ntel/buildbot/start.sh
@@ -8,5 +8,11 @@ BUILDBOT_DATA=${BUILDBOT_DATA-"/buildbot"}
 
 ssh-keygen -q -b ${SSH_BITS} -t ${SSH_TYPE} -N '' -f ${SSH_FILE} 0>&- 1>/dev/null 2>/dev/null
 
+if [ -n "$BUILDBOT_KNOWN_HOSTS_FILE" ] && [ -f "$BUILDBOT_KNOWN_HOSTS_FILE" ]
+then
+    mkdir -p "${HOME}/.ssh"
+    ln -s "$BUILDBOT_KNOWN_HOSTS_FILE" "${HOME}/.ssh/known_hosts"
+fi
+
 ln -v -s -f ${HOME}/buildbot.tac $BUILDBOT_DATA
 exec twistd --nodaemon --logfile=- --pidfile=/tmp/twistd.pid --python=buildbot.tac
