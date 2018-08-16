79f57d14dd3a0478e92a8aeb021d6c71e128c9fe
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-08-16 09:17:44 -0500

    Populate buildbot with gitlab public keys

--- docker/buildbot-master/buildbot/start.sh
+++ docker/buildbot-master/buildbot/start.sh
@@ -6,5 +6,13 @@ SSH_FILE=${SSH_FILE-"$HOME/.ssh/id_rsa"}
 
 ssh-keygen -q -b ${SSH_BITS} -t ${SSH_TYPE} -N '' -f ${SSH_FILE} 0>&- 1>/dev/null 2>/dev/null
 
+if [ -n "$BUILDBOT_KNOWN_HOSTS_FILE" ] && [ -f "$BUILDBOT_KNOWN_HOSTS_FILE" ]
+then
+    install -v -D \
+        -o `id -u` -g `id -g` -m 0600 \
+        "$BUILDBOT_KNOWN_HOSTS_FILE" \
+        "${HOME}/.ssh/known_hosts"
+fi
+
 # Run original buildbot entrypoint
 exec "/usr/src/buildbot/docker/start_buildbot.sh"
